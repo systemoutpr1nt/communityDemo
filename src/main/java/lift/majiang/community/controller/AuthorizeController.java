3bf563662b48efe6b503173fbe22b59a949decbb
xiaoyu <13603780983@163.com>
2020-03-17 20:04:15 +0800

    into cookie

--- src/main/java/lift/majiang/community/controller/AuthorizeController.java
+++ src/main/java/lift/majiang/community/controller/AuthorizeController.java
@@ -12,7 +12,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 @Controller
@@ -34,7 +36,7 @@ public class AuthorizeController {
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
                            @RequestParam(name="state")String state,
-                                   HttpServletRequest request){
+                                   HttpServletResponse response){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -46,13 +48,16 @@ public class AuthorizeController {
         if (githubUser != null){
             //登陆成功，写cookie，和session
             User user = new User();
-            user.setToken(UUID.randomUUID().toString());
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
             user.setName(githubUser.getName());
             user.setAccountId(String.valueOf(githubUser.getId()));
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(user.getGmtCreate());
             userMapper.insert(user);
-            request.getSession().setAttribute("user",githubUser);
+
+            response.addCookie(new Cookie("token",token));
+
             return "redirect:/";
         }else{
             //登陆失败
--- src/main/java/lift/majiang/community/controller/IndexController.java
+++ src/main/java/lift/majiang/community/controller/IndexController.java
@@ -1,15 +1,45 @@
 package lift.majiang.community.controller;
 
+import lift.majiang.community.mapper.UserMapper;
+import lift.majiang.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 class IndexController {
 
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/")
-    public String greeting()  {
+    public String greeting(HttpServletRequest request)  {
+    Cookie[] cookies = request.getCookies();
+    for (Cookie cookie : cookies){
+        if (cookie.getName().equals("token")){
+            String token = cookie.getValue();
+            User user = userMapper.findByToken(token);
+            if (user != null){
+                request.getSession().setAttribute("user",user);
+            }
+            break;
+        }
+    }
+
+
+
+
+
+
+
+
+
+
         return "index";
     }
 
