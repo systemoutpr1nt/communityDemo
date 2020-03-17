d1f9dc6f4b4d4a6096ef23866806ea55aa82e251
xiaoyu <13603780983@163.com>
2020-03-17 19:30:12 +0800

    join database

---
+++ src/main/java/lift/majiang/community/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package lift.majiang.community.mapper;
+
+import lift.majiang.community.model.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Mapper
+public interface UserMapper {
+    @Insert("insert into USER(name,account_id,token,gmt_create,gmt_modified) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
+    public void insert(User user);
+}
