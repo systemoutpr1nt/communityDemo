00fe137328d2bae62d114ca4f62dbc737f906e6b
xiaoyu <13603780983@163.com>
2020-03-17 12:02:24 +0800

    extract github config from code to properties

---
+++ src/main/java/lift/majiang/community/provider/GithubProvider.java
@@ -0,0 +1,48 @@
+package lift.majiang.community.provider;
+
+import com.alibaba.fastjson.JSON;
+import lift.majiang.community.dto.AccessTokenDTO;
+import lift.majiang.community.dto.GithubUser;
+import okhttp3.*;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class GithubProvider {
+    public String getAccessToken(AccessTokenDTO accessTokenDTO){
+        MediaType mediaType = MediaType.get("application/json; charset=utf-8");
+        OkHttpClient client = new OkHttpClient();
+        RequestBody body = RequestBody.create(mediaType, JSON.toJSONString(accessTokenDTO));
+        Request request = new Request.Builder()
+                .url("https://github.com/login/oauth/access_token")
+                .post(body)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            String string = response.body().string();
+            String token = string.split("&")[0].split("=")[1];
+            return token;
+        } catch (Exception e) {
+        e.printStackTrace();
+        }
+        return null;
+    }
+    public GithubUser getUser(String accessToken){
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://api.github.com/user?access_token="+accessToken)
+                .build();
+        try  {Response response = client.newCall(request).execute();
+            String string = response.body().string();
+            GithubUser githubUser = JSON.parseObject(string, GithubUser.class);
+            //将对象string中的数据自动解析到GithubUser.class中对应的id和name
+            return githubUser;
+        }catch (IOException e){
+
+        }return  null;
+    }
+
+
+
+
+}
